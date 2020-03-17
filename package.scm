9db1fb70227f1842cf7205505398d4880ea7a778
Justin Ethier <justin.ethier@gmail.com>
2020-03-17 17:29:46 -0400

    Rev++

--- package.scm
+++ package.scm
@@ -1,6 +1,6 @@
 (package
   (name char-set)
-  (version 0.1)
+  (version 0.2)
   (license "BSD")
   (authors "Arthur Maciel <arthurmaciel at gmail dot com>")
   (maintainers "Arthur Maciel <arthurmaciel at gmail dot com>")
